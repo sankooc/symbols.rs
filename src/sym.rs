8da46be6bf9b85991336bc077e02f7ac863c1f5e
sankooc <sankooc@163.com>
2024-05-23 16:06:11 +0800

    fx display

--- src/sym.rs
+++ src/sym.rs
@@ -1,19 +1,26 @@
 use std::str;
 
 use include_assets::{include_dir, NamedArchive};
+use term_grid::{Grid, GridOptions, Direction, Filling, Cell};
+
 
 pub fn load(token: &str) {
     let archive = NamedArchive::load(include_dir!("assets"));
     let lns = str::from_utf8(archive.get("data.txt").unwrap()).unwrap().lines();
+    let mut grid = Grid::new(GridOptions {
+        filling:     Filling::Spaces(2),
+        direction:   Direction::LeftToRight,
+    });
     let _ = lns.for_each(|x| {
         let inx = x.find(token);
         match inx {
             Some(_inx) => {
                 let v: Vec<&str> = x.split('|').collect();
-                print!("[\\{}]  {} \t", v[1], v[2]);
+                grid.add(Cell::from(v[1]));
+                grid.add(Cell::from(v[2]));
             },
             None => {}
         }
     });
-    print!("\r\n");
+    println!("{}", grid.fit_into_width(100).unwrap());
 }
